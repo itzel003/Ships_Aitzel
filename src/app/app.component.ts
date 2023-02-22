7a1a7104994fcafa22da2f1c64ed0675e73e89f6
Itzel Arias <a3521110602@alumno.uttehuacan.edu.mx>
2023-02-22 08:02:02 -0600

    segundo push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'space_avei';
+  data: any [] = [];
+
+
+constructor(
+    private http: HttpClient
+  )
+
+  {}
+
+  ngOnInit() {
+  this.http.get('https://api.spacexdata.com/v4/ships').subscribe((data: any) => {
+    this.data = data;
+    
+  })
 }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export class data{
+  roles:String;
+  link:String;
+  image:String;
+  launches:String;
+  home_port:String;
+  name:String;
+  
+  constructor(    
+    roles:String,
+    link:String,
+    image:String,
+    launches:String,
+    home_port:String,
+    name:String
+)
+
+{
+  this.roles=roles;
+  this.link=link;
+  this.image=image;
+  this.launches=launches;
+  this.home_port=home_port;
+  this.name=name;
+}
+  }
+ 
+  
